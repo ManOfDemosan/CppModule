fff776a23bcd483dec3c6d39ff3627f202783cab
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-10-28 16:45:37 +0900

    cpp00

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,1 +1,15 @@
-#include <iostream>
+#include "Contact.hpp"
+
+std::string Contact::getFirstName() const
+{
+	return this->_firstName;
+}
+
+int main() {
+	Contact home;
+	Contact mobile;
+
+
+	home.getFirstName();
+	mobile.getFirstName();
+}
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,11 +1,20 @@
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
+#include<iostream>
+
 class Contact
 {
 	private:
-	
+		// std::string index; 이거는 폰북으로 index로 넘어가야한다.
+		std::string _firstName;
+		std::string _lastName;
+		std::string _nickName;
+		std::string _phoneNum;
+		std::string _secret;
 	public:
+		std::string getFirstName() const;
+
 };
 
 #endif
