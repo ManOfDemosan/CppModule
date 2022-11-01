71783ccabad0654c45cba8da8f9a43d599c88d17
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-11-01 22:49:26 +0900

    Phonebook

--- cpp00/ex01/test.cpp
+++ cpp00/ex01/test.cpp
@@ -3,8 +3,13 @@
 
 int main ()
 {
-	std::cout << std::setw(50) << std::setfill('=') << "=" << std::endl;
-	std::cout << std::setw(50) << std::setfill('=') << std::endl;
-
-
+	// std::cout << std::setw(50) << std::setfill('=') << "=" << std::endl;
+	// std::cout << std::setw(50) << std::setfill('=') << std::endl;
+	std::cout << std::setw(43) << std::setfill('=') << "=" << std::endl;
+	std::cout << std::setfill(' ') << std::setw(10) << "INDEX" << "|";
+	std::cout << std::setw(10) << "FIRST NAME" << "|";
+	std::cout << std::setw(10) << "LAST NAME" << "|";
+	std::cout << std::setw(10) << "NICKNAME" << std::endl;
+	std::cout << std::setw(43) << std::setfill('-') << "-" << std::endl;
+	// std::cout << std::setfill(' ');	
 }
