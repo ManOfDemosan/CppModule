f083511c4377edf43981d290f1b81956282159da
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-11-04 14:22:58 +0900

    ex02_F

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <iomanip>
 
 int main(){
 	std::string		str = "HI THIS IS BRAIN";
 	std::string		*stringPTR = &str;
 	std::string		&stringREF = str;
+	
+	std::cout << std::setw(49) << std::setfill('=') << '=' << std::endl;
+	std::cout << &str << " is address of the string variable" << std::endl;
+	std::cout << stringPTR << " is address held by stringPTR" << std::endl;
+	std::cout << &stringREF << " is address held by stringREF" << std::endl;
+	std::cout << std::setw(49) << std::setfill('=') << '=' << std::endl;
+	std::cout << str << " is value of the string variable"<< std::endl;
+	std::cout << *stringPTR << " is value pointed to by stringPTR"<< std::endl;
+	std::cout << stringREF << " is value of the string variable"<< std::endl;
+	std::cout << std::setw(49) << std::setfill('=') << '=' << std::endl;
+
 }
