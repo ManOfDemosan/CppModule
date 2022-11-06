5cb72143db753220621286b11f4c0a205ccdc9b6
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-11-06 23:15:34 +0900

    cpp01

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -1,6 +1,6 @@
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon) :_name(name), _weapon(weapon) {
+HumanA::HumanA(std::string name, Weapon &weapon) :_name(name), _weapon(weapon) { // 초기화 리스트
 }
 
 HumanA::~HumanA(){
