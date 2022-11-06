b2369e4a6c04110ec563fde2beb765195536ca77
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-11-06 23:05:42 +0900

    cpp01

--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,7 +6,7 @@ int main()
 	{
 		Weapon club = Weapon("crude spiked club");
 
-		HumanA bob("Bob", club);
+		HumanA bob("Bob", club);//club을 넘길때 참조자로 넘겨서 사용 그렇지 않으면 복사가 되어 사용됨
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
