4289acf8427cd3ff27388a3cd28845762b6b93ae
ManOfDaemosan <jaehwkim@c2r5s2.42seoul.kr>
2022-11-20 16:58:21 +0900

    05_00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,8 +1,6 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() {
-    this->_name = "마키아벨리";
-    this->_grade = 1;
+Bureaucrat::Bureaucrat(): _name("마키아벨리"), _grade(1) {
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& obj) : _name(obj.getName()) , _grade(obj.getGrade()){
@@ -32,6 +30,11 @@ unsigned int    Bureaucrat::getGrade() const{
 std::string Bureaucrat::getName() const{
     return(this->_name);
 }
+
+const char * Bureaucrat::GradeTooHighException
+
+
+
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bu){
     return os << bu.getName() << " , bureaucrat grade " <<bu.getGrade() << std::endl; 
 }
