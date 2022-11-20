166668b1206e8ad97cf0c8129603f5644abbfcb8
ManOfDaemosan <jaehwkim@c2r5s2.42seoul.kr>
2022-11-20 16:53:09 +0900

    05_00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,11 +1,18 @@
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat() {
-
+    this->_name = "마키아벨리";
+    this->_grade = 1;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& obj) {
+Bureaucrat::Bureaucrat(const Bureaucrat& obj) : _name(obj.getName()) , _grade(obj.getGrade()){
 
+}
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : _name(name), _grade(grade){
+    if(_grade > 150)
+        throw GradeTooLowException();
+    else if(_grade < 1)
+        throw GradeTooHighException();
 }
 
 Bureaucrat::~Bureaucrat() {
@@ -13,5 +20,18 @@ Bureaucrat::~Bureaucrat() {
 }
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& obj) {
+    this->_name = obj.getName();
+    this->_grade = obj.getGrade();
+    return (*this);
+}
 
+unsigned int    Bureaucrat::getGrade() const{
+    return(this->_grade);
+}
+
+std::string Bureaucrat::getName() const{
+    return(this->_name);
+}
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bu){
+    return os << bu.getName() << " , bureaucrat grade " <<bu.getGrade() << std::endl; 
 }
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -10,8 +10,22 @@ class Bureaucrat {
  public:
   Bureaucrat();
   Bureaucrat(const Bureaucrat& obj);
+  Bureaucrat(std::string name, unsigned int grade);//뉴
   ~Bureaucrat();
   Bureaucrat& operator=(const Bureaucrat& obj);
+
+  std::string  getName() const;
+  unsigned int  getGrade() const;
+
+  class GradeTooLowException : public std::exception{
+    const char * what() const throw();
+  };
+
+  class GradeTooHighException : public std::exception{
+    const char * what() const throw();
+  };
 };
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bu);
 
 #endif
