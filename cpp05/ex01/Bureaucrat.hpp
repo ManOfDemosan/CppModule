f5443e07254d5d46fa77ba5f5858dc35b4c09036
ManOfDaemosan <jaehwkim@c4r9s4.42seoul.kr>
2022-11-22 13:05:39 +0900

    05_01

---
+++ cpp05/ex01/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+# include <iostream>
+
+class Bureaucrat {
+ private:
+	std::string	_name;
+	unsigned int _grade;
+ public:
+  Bureaucrat();
+  Bureaucrat(const Bureaucrat& obj);
+  Bureaucrat(std::string name, unsigned int grade);
+  ~Bureaucrat();
+  Bureaucrat& operator=(const Bureaucrat& obj);
+
+  virtual std::string  getName() const;
+  unsigned int  getGrade() const;
+  void  upGrade();//등급이 올라가려면 마이너스
+  void  deGrade();//등급이 떨어지려면 쁠러스
+
+  class GradeTooLowException : public std::exception{
+    const char * what() const throw();
+  };
+
+  class GradeTooHighException : public std::exception{
+    const char * what() const throw();
+  };
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& bu);
+
+#endif
