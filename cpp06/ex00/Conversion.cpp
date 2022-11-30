26a65f410977fdea1b407e125f7f57a5ed305639
ManOfDaemosan <jaehwkim@c4r8s2.42seoul.kr>
2022-11-30 19:58:50 +0900

    06_00

--- cpp06/ex00/Conversion.cpp
+++ cpp06/ex00/Conversion.cpp
@@ -26,21 +26,17 @@ double Conversion::getParamValue() const {
 }
 
 Conversion::Conversion(std::string param): _param(param), _paramValue(0.0), _check(false){
-    try{
-        char *ptr = NULL;
-        _paramValue = std::strtod(_param.c_str(), &ptr);
-        if(_paramValue == 0.0 && (_param[0] != '-' && _param[0] != '+' && !std::isdigit(_param[0]))){
-            _check = true;
-            return ;
-        }
-        if(*ptr && std::strcmp(ptr, "f")){
-            _check = true;
-            return ;
-        }
+    char *ptr = NULL;
+     _paramValue = std::strtod(_param.c_str(), &ptr);
+    if(_paramValue == 0.0 && (_param[0] != '-' && _param[0] != '+' && !std::isdigit(_param[0]))){
+        _check = true;
+        return ;
     }
-    catch(const std::exception& e){
-        std::cout << e.what() << std::endl;
+    if(*ptr && std::strcmp(ptr, "f")){
+        _check = true;
+        return ;
     }
+
 }
 
 char Conversion::toChar() const {
@@ -65,6 +61,8 @@ void Conversion::printChar(std::ostream &os) const {
 		os << "impossible" << std::endl;
 	} else if (std::isprint(toChar())) {
 		os << "'" << toChar() << "'" << std::endl;
+	} else if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
+		os << "'" << _param << "'" << std::endl;
 	} else {
 		os << "non displayable" << std::endl;
 	}
@@ -74,24 +72,30 @@ void Conversion::printInt(std::ostream &os) const {
 	os << "int: ";
 	if (_paramValue != _paramValue || _paramValue > std::numeric_limits<int>::max() || _paramValue < std::numeric_limits<int>::min()) {
 		os << "impossible" << std::endl;
+	} else if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
+		os << static_cast<int>(_param[0]) << std::endl;
 	} else {
 		os << toInt() << std::endl;
 	}
 }
 
 void Conversion::printFloat(std::ostream &os) const {
-	if (toFloat() == static_cast<int64_t>(toFloat())) {
+	if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
+		os << "float: "<< static_cast<float>(_param[0]) << "f" << std::endl;
+	} else if (toFloat() == static_cast<int64_t>(toFloat())) {
 		os << "float: " << toFloat() << ".0f" << std::endl;
 	} else {
-		os << "float: " << std::setprecision(std::numeric_limits<float>::digits10) << toFloat() << "f" << std::endl;
+		os << "float2: " << std::setprecision(std::numeric_limits<float>::digits10) << toFloat() << "f" << std::endl;
 	}
 }
 
 void Conversion::printDouble(std::ostream &os) const {
-	if (toDouble() == static_cast<int64_t>(toDouble())) {
+	if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
+		os << "double: " << static_cast<double>(_param[0]) << ".0" << std::endl;
+	}else if (toDouble() == static_cast<int64_t>(toDouble())) {
 		os << "double: " << toDouble() << ".0" << std::endl;
 	} else {
-		os << "double: " << std::setprecision(std::numeric_limits<double>::digits10) << toDouble() << std::endl;
+		os << "double2: " << std::setprecision(std::numeric_limits<double>::digits10) << toDouble() << std::endl;
 	}
 }
 
@@ -102,14 +106,10 @@ const char* Conversion::ImpossibleException:: what() const throw(){
 }
 
 std::ostream& operator<<(std::ostream& os, const Conversion& conversion) {
-	try{
-        conversion.printChar(os);
-        conversion.printInt(os);
-        conversion.printFloat(os);
-        conversion.printDouble(os);
-    }
-        catch(const std::exception& e){
-        std::cout << e.what() << std::endl;
-    }
+
+    conversion.printChar(os);
+    conversion.printInt(os);
+    conversion.printFloat(os);
+    conversion.printDouble(os);
 	return os;
 }
