e5a7ccf2fed0658faad8a1c2de75cb9c6613c41c
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-12-02 13:07:03 +0900

    06_00

--- cpp06/ex00/Conversion.cpp
+++ cpp06/ex00/Conversion.cpp
@@ -23,7 +23,7 @@ double Conversion::getParamValue() const {
 
 Conversion::Conversion(std::string param): _param(param), _paramValue(0.0){
     char *ptr = NULL;
-     _paramValue = std::strtod(_param.c_str(), &ptr);
+    _paramValue = std::strtod(_param.c_str(), &ptr);
 }
 
 char Conversion::toChar() const {
@@ -44,7 +44,7 @@ double Conversion::toDouble() const {
 
 void Conversion::printChar(std::ostream &os) const {
 	os << "char: ";
-	if (_paramValue != _paramValue || _paramValue > std::numeric_limits<char>::max() || _paramValue < std::numeric_limits<char>::min()) {
+	if (_paramValue > std::numeric_limits<char>::max() || _paramValue < std::numeric_limits<char>::min()) {
 		os << "impossible" << std::endl;
 	} else if (std::isprint(toChar())) {
 		os << "'" << toChar() << "'" << std::endl;
@@ -57,7 +57,7 @@ void Conversion::printChar(std::ostream &os) const {
 
 void Conversion::printInt(std::ostream &os) const {
 	os << "int: ";
-	if (_paramValue != _paramValue || _paramValue > std::numeric_limits<int>::max() || _paramValue < std::numeric_limits<int>::min()) {
+	if (_paramValue > std::numeric_limits<int>::max() || _paramValue < std::numeric_limits<int>::min()) {
 		os << "impossible" << std::endl;
 	} else if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
 		os << static_cast<int>(_param[0]) << std::endl;
@@ -68,9 +68,9 @@ void Conversion::printInt(std::ostream &os) const {
 
 void Conversion::printFloat(std::ostream &os) const {
 	if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
-		os << "float: "<< static_cast<float>(_param[0]) << "f" << std::endl;
+		os << "float0: "<< static_cast<float>(_param[0]) << "f" << std::endl;
 	} else if (toFloat() == static_cast<int64_t>(toFloat())) {
-		os << "float: " << toFloat() << ".0f" << std::endl;
+		os << "float1: " << toFloat() << ".0f" << std::endl;
 	} else {
 		os << "float2: " << std::setprecision(std::numeric_limits<float>::digits10) << toFloat() << "f" << std::endl;
 	}
@@ -78,11 +78,11 @@ void Conversion::printFloat(std::ostream &os) const {
 
 void Conversion::printDouble(std::ostream &os) const {
 	if (_param.length() == 1 && !isdigit(static_cast<char>(_param[0]))){
-		os << "double: " << static_cast<double>(_param[0]) << ".0" << std::endl;
+		os << "double0: " << static_cast<double>(_param[0]) << ".0" << std::endl;
 	}else if (toDouble() == static_cast<int64_t>(toDouble())) {
-		os << "double: " << toDouble() << ".0" << std::endl;
+		os << "double1: " << toDouble() << ".0" << std::endl;
 	} else {
-		os << "double: " << std::setprecision(std::numeric_limits<double>::digits10) << toDouble() << std::endl;
+		os << "double2: " << std::setprecision(std::numeric_limits<double>::digits10) << toDouble() << std::endl;
 	}
 }
 
