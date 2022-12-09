cc1f9851dc6620f770fa0b93942997cb3be65b89
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-12-09 15:38:18 +0900

    08_00

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <vector>
+
+template<typename T>
+typename T::iterator easyfind(T& container, int findVal){
+	typename T::iterator i = std::find(container.begin(), container.end(), findVal);
+	if (i == container.end())
+		throw std::runtime_error("히잉 찾는거 없어잉");
+	return i;
+}
+
+#endif
---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,58 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <stack>
+
+
+int main(){
+	std::vector<int> test1;
+	std::deque<int> test2;
+	std::list<int> test3;
+	
+	int value;
+	srand(time(NULL));
+
+
+	std::cout << "랜덤 값 0 ~ 9까지 입력" << std::endl;
+	for (int idx = 0; idx < 10; idx++) {
+		value = rand() % 10;
+		std::cout << value << std::endl;
+		test1.push_back(value);
+		test2.push_back(value);
+		test3.push_back(value);
+	}
+	std::cout << "==========vector==========" << std::endl;
+	try {
+		int val = 1;
+		std::cout << val << "찾을껴" << std::endl;
+		std::cout << *(easyfind(test1, val)) << "있다!" << std::endl;
+	} catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "==========deque===========" << std::endl;
+	try {
+		int val = 2;
+		std::cout << val << "찾을껴" << std::endl;
+		std::cout << *(easyfind(test2, val)) << "있다!" << std::endl;
+	} catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << "===========list============" << std::endl;
+	try {	
+		int val = 3;
+		std::cout << val << "찾을껴" << std::endl;
+		std::cout << *(easyfind(test3, val)) << "있다!" << std::endl;
+	} catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
+	// std::cout << "값 7의 위치는? : " << easyfind(test1, 7) << std::endl;
+	// std::cout << "값 11의 위치는? : " << easyfind(test1, 11) << std::endl;
+	// std::cout << "test2" << std::endl;
+	// printVector(test2);
+	// std::cout << "값 7의 위치는? : " << easyfind(test2, 7) << std::endl;
+	// std::cout << "값 11의 위치는? : " << easyfind(test2, 11) << std::endl;
+	// std::cout << "test3" << std::endl;
+	// printVector(test3);
+	// std::cout << "값 7의 위치는? : " << easyfind(test3, 7) << std::endl;
+	// std::cout << "값 11의 위치는? : " << easyfind(test3, 11) << std::endl;
+}
