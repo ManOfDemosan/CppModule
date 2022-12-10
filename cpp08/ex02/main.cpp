26bcad351b9296a675ce6c8092eb65f4bc99fd80
김재환 <kimjaehwan@gimjaehwan-ui-MacBookAir.local>
2022-12-10 15:57:08 +0900

    08_02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -5,15 +5,15 @@
 int main()
 {
 MutantStack<int> mstack;
-mstack.push(5);
+mstack.push(2);
 mstack.push(17);
 std::cout << mstack.top() << std::endl;
 mstack.pop();
-std::cout << mstack.size() << std::endl;
+std::cout <<"제일 위에있는 17이 날라감"<<std::endl;
+std::cout << "stack 의 size는 "<< mstack.size() << std::endl;
 mstack.push(3);
 mstack.push(5);
 mstack.push(737);
-//[...]
 mstack.push(0);
 MutantStack<int>::iterator it = mstack.begin();
 MutantStack<int>::iterator ite = mstack.end();
